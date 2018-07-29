d8f49926bf3c3d12ca3877d20c7bdabb6b5844bc
Matt Burdan <burdz@burdz.net>
2018-07-29 05:39:13 -0700

    --user flag for pip modules

--- bin/pip-mod-upgrade.py
+++ bin/pip-mod-upgrade.py
@@ -4,4 +4,4 @@ import pip
 from subprocess import call
 
 for dist in pip.get_installed_distributions():
-    call("pip install --upgrade " + dist.project_name, shell=True)
+    call("pip install --upgrade --user " + dist.project_name, shell=True)
