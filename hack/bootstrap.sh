d287711c106e232d4a091272ea4170d721485fe6
Matt Burdan <burdz@burdz.net>
2018-08-15 16:50:41 -0700

    update `wtf` download link

--- hack/bootstrap.sh
+++ hack/bootstrap.sh
@@ -311,7 +311,7 @@ bat() {
 }
 
 wtf() {
-    curl -fsSL -o wtf-${WTF_VERSION}.tar.gz "https://github.com/senorprogrammer/wtf/releases/download/v${WTF_VERSION}/wtf_${WTF_VERSION}_linux_amd64.tar.gz"
+    curl -fsSL -o wtf-${WTF_VERSION}.tar.gz "https://github.com/senorprogrammer/wtf/releases/download/${WTF_VERSION}/wtf_${WTF_VERSION}_linux_amd64.tar.gz"
     tar -zxvf wtf-${WTF_VERSION}.tar.gz
     sudo cp wtf_${WTF_VERSION}_linux_amd64/wtf /usr/local/bin/
     sudo rm -rf wtf-* wtf_*
