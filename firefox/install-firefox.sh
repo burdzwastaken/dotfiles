dff20d66db5b288b9719851bfe07ce2bf94a5ef2
Matt Burdan <burdz@burdz.net>
2019-12-17 22:55:45 -0800

    add some notes when installing firefox

--- firefox/install-firefox.sh
+++ firefox/install-firefox.sh
@@ -9,6 +9,9 @@
 # install firefox
 #------------------------------------------------------------------------------
 
-curl -fsSL -o firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US"
+echo 'downloading firefox tarball...'
+curl -fSL -o firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US"
+echo 'extracting to /opt'
 sudo tar -C /opt -xvf firefox.tar.bz2
+echo 'cleaning up...'
 rm -rf firefox.tar.bz2
