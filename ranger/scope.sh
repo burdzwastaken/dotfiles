2f50abbc5a900c9b478970bdf8205db2b470606e
Matt Burdan <burdz@burdz.net>
2020-02-20 20:06:15 -0800

    [misc] updates for coc, bash & ranger (#94)

--- ranger/scope.sh
+++ ranger/scope.sh
@@ -8,7 +8,7 @@ IFS=$'\n'
 # ANSI color codes are supported.
 # STDIN is disabled, so interactive scripts won't work properly
 
-# This script is considered a configuration file and must be updated manually.
+# This script is considered a config file and must be updated manually.
 # It will be left untouched if you upgrade ranger.
 
 # Meanings of exit codes:
