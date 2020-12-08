417f295d66774f41a005d77870a45a8a3b4e310d
Matt Burdan <burdz@burdz.net>
2020-12-08 05:29:31 -0800

    update spotify gpg key

--- hack/bootstrap.sh
+++ hack/bootstrap.sh
@@ -64,7 +64,7 @@ repos-gpg() {
     curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - #gcloud-sdk
     curl -fsSL https://s3.amazonaws.com/download.draios.com/DRAIOS-GPG-KEY.public | sudo apt-key add - #sysdig
     curl -fsSL https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add - #signal
-    curl -fsSL https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - #spotify
+    curl -fsSL https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - #spotify
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 1484120AC4E9F8A1A577AEEE97A80C63C9D8B80B #osquery
     if [ -z "$IN_DOCKER" ]; then
         sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 #rvm
