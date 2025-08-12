8a7a1166f2a45013444f5be78d2c18dedb4b8e49
Matt Burdan <burdz@burdz.net>
2025-08-12 20:04:19 +0800

    [opencode] Bumpy

--- home/packages/opencode.nix
+++ home/packages/opencode.nix
@@ -1,12 +1,12 @@
 { unstable, opencode-src }:
 
 unstable.opencode.overrideAttrs (old: {
-  version = "0.3.122";
+  version = "0.4.26";
   src = opencode-src;
   node_modules = old.node_modules.overrideAttrs (nmOld: {
-    outputHash = "sha256-oZa8O0iK5uSJjl6fOdnjqjIuG//ihrj4six3FUdfob8=";
+    outputHash = "sha256-ql4qcMtuaRwSVVma3OeKkc9tXhe21PWMMko3W3JgpB0=";
   });
   tui = old.tui.overrideAttrs (tuiOld: {
-    vendorHash = "sha256-qsOL6gsZwEm7YcYO/zoyJAnVmciCjPYqPavV77psybU=";
+    vendorHash = "sha256-jINbGug/SPGBjsXNsC9X2r5TwvrOl5PJDL+lrOQP69Q=";
   });
 })
