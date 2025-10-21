398089772674e96af097bec1854d5f82cb730f80
Matt Burdan <burdz@burdz.net>
2025-10-21 21:41:15 +0800

    [clis] Bumpy opencode

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
 
     # use an overridden version for now https://github.com/sst/opencode/issues/462
     opencode = {
-      url = "github:sst/opencode/v0.15.8";
+      url = "github:sst/opencode/v0.15.10";
       flake = false;
     };
   };
--- home/packages/opencode.nix
+++ home/packages/opencode.nix
@@ -1,10 +1,10 @@
 { unstable, opencode-src }:
 
 unstable.opencode.overrideAttrs (old: {
-  version = "0.15.4";
+  version = "0.15.10";
   src = opencode-src;
   node_modules = old.node_modules.overrideAttrs (nmOld: {
-    outputHash = "sha256-EfH8fBgP0zsKVu26BxFq1NCwWLG6vlOhDD/WQ7152hA=";
+    outputHash = "sha256-iJbflfKwDwKrJQgy5jxrEhkyCie2hsEMmiLf2btE60E=";
   });
   tui = old.tui.overrideAttrs (tuiOld: {
     vendorHash = "sha256-g3+2q7yRaM6BgIs5oIXz/u7B84ZMMjnxXpvFpqDePU4=";
