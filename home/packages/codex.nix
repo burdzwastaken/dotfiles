1acfcccf004befbb5736e2da0b1cd820555b0883
Matt Burdan <burdz@burdz.net>
2025-11-14 19:03:42 +0800

    [clis] Bumpy codex

--- home/packages/codex.nix
+++ home/packages/codex.nix
@@ -2,17 +2,17 @@
 
 unstable.rustPlatform.buildRustPackage rec {
   pname = "codex";
-  version = "0.57.0";
+  version = "0.58.0";
 
   src = unstable.fetchFromGitHub {
     owner = "openai";
     repo = "codex";
     tag = "rust-v${version}";
-    hash = "sha256-Mjn2SesclOTBLiE7hQRtdyI/TpIM5lw2uswYyCMhyGY=";
+    hash = "sha256-CY9ai6Ia6bixACEIIWc85DqFDUf31zMnVJTqkVQArDs=";
   };
 
   sourceRoot = "${src.name}/codex-rs";
-  cargoHash = "sha256-ijXcYBMP63VzeHqVTEebJ83cYQtQgHU62kWklA1NHEA=";
+  cargoHash = "sha256-FqGgHzq9JhegpRQEpOGtTmdnfypOeIlE43SdWs1/ddM=";
 
   nativeBuildInputs = with unstable; [
     installShellFiles
